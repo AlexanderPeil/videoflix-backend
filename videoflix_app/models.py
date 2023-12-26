2ad49919e30e7b84fd9484af9756da8f9d4ce9e4
Alexander Peil <121516867+SaschkaSCF@users.noreply.github.com>
2023-12-26 19:16:41 +0100

    first commit

---
+++ videoflix_app/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from datetime import date
+
+
+class Video(models.Model):
+    created_at = models.DateField(default=date.today)
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=500)
+    video_file = models.FileField(uploaded_to='videos', blank=True, null=True)
+
+    def __str__(self):
+        return self.title
