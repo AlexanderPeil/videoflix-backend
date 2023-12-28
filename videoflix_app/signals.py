35a1cae81914abf2f1cbb247987e66e1723f9322
Alexander Peil <121516867+SaschkaSCF@users.noreply.github.com>
2023-12-28 04:12:39 +0100

    added taks.py, extended signals.py and installed redis

--- videoflix_app/signals.py
+++ videoflix_app/signals.py
@@ -1,4 +1,5 @@
 from django.db.models.signals import post_save, post_delete
+from videoflix_app.tasks import convert_480p
 from .models import Video
 from django.dispatch import receiver
 import os
@@ -6,8 +7,10 @@ import os
 
 @receiver(post_save, sender=Video)
 def video_post_save(sender, instance, created, **kwargs):
+    print('Video saved.')
     if created:
         print("New video created.")
+        convert_480p(instance.video_file.path)
 
 
 @receiver(post_delete, sender=Video)
